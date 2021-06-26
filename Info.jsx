e7ead8f79abeef07b233f00370581df0339ed259
pradeeppppppp <86526628+pradeeppppppp@users.noreply.github.com>
2021-06-26 20:51:07 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Stock marketing</h1>
+      <p>crypto Exchange on binance</p>
+    </div>
+  );
+}
+
+export default Info;
